a50e3d161f4f053e4ef97b30a4d86937209e23df
Aravinth <aravinthgoodwill@gmail.com>
2019-04-30 10:11:38 +0100

    updates..

--- App.js
+++ App.js
@@ -14,8 +14,8 @@ export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Image style={{width:175,height:168}} source={require('./wow.png')}/>
-        <Text style={styles.welcome}>ReactNative with CodePush!</Text>
+        {/*<Image style={{width:175,height:168}} source={require('./wow.png')}/> */}
+        <Text style={styles.welcome}>ReactNative with CodePush</Text>
         <Text style={styles.codePushText1}>This is On-Air CodePush Demo</Text>
         <Image style={{width: 290, height: 290}} source={require('./noUpdate.png')}/>
         {/*<Image style={{width: 350, height: 250}} source={require('./image.png')}/>
